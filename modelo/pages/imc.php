06d1de5c772d2ec0019062e3b5d90e321ba84be3
lcsrlm <lucas_rosa-lima@estudante.sesisenai.org.br>
2023-10-02 09:49:58 -0300

    feat: adiciona calculadora de IMC

--- modelo/pages/imc.php
+++ modelo/pages/imc.php
@@ -1,11 +1,29 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <title>Calculadora de IMC</title>
 </head>
+<style>
+    :root {
+        font-family: sans-serif;
+    }
+</style>
+
 <body>
-  imc
+    <h1>Calculadora de IMC</h1>
+    <form action="calcular_imc.php" method="POST">
+        <label for="peso">Peso (em quilogramas):</label>
+        <input type="number" step="0.01" name="peso" id="peso" required>
+        <br>
+        <br>
+        <label style="margin-left: 20px;" for="altura">Altura (em metros):</label>
+        <input style="margin-right: 10px" type="number" step="0.01" name="altura" id="altura" required>
+        <br>
+        <br>
+        <button style="margin-left: 10%;" type="submit">Calcular IMC</button>
+    </form>
 </body>
+
 </html>
