d725126d599c22a00e0473a9231fffd7127b6c81
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-10-02 12:38:09 -0500

    "feat: ex4.tabuada"

--- Eliana/pages/calculadora.php
+++ Eliana/pages/calculadora.php
@@ -1,13 +1,16 @@
-<?php
-$codigo= filter_input(INPUT_POST, 'codigo', FILTER_VALIDATE_FLOAT);
-$quantidade= filter_input(INPUT_POST, 'quantidade', FILTER_VALIDATE_FLOAT);
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<form method="post" action="resultado.php">
+    <label>Qual a tabuada que você quer ?</label><br />
+    <input type="number" name="valor"/><br />
 
-if(!$codigo || $quantidade){
-    header('Location:lanchonete.php?error=true');
-}
-
-//var_dump($peso);
-
-$resultado= $codigo + $quantidade;
-echo "Valor do frete e $resultado";
-?>
+    <button type="submit">Calcular</button>
+  </form>
+</body>
+</html>
