eeeaeb4ea53e9e1c920c78e5903852fb60c9768c
lcsrlm <lucas_rosa-lima@estudante.sesisenai.org.br>
2023-10-03 08:22:18 -0300

    feat: adiciona inicio do exercicio 04

--- modelo/pages/calculadora.php
+++ modelo/pages/calculadora.php
@@ -6,6 +6,23 @@
   <title>Document</title>
 </head>
 <body>
-  calculadora
+  <h1>Calculadora</h1>
+  <form action="resultado.php" method="POST">
+    <label for="valor1">1º número</label>
+    <input type="number" name="valor1" id="valor1" required>
+    <br><br>
+    <label for="valor2">2º número</label>
+    <input type="number" name="valor2" id="valor2" required>
+    <br><br>
+    <label for="operacao">Selecione a operação</label>
+    <select name="operacao" id="operacao" required>
+      <option value="soma">Soma (+)</option>
+      <option value="subtracao">Subtração (-)</option>
+      <option value="multiplicacao">Multiplicação (*)</option>
+      <option value="divisao">Divisão (/)</option>
+    </select>
+    <br><br>
+    <button type="submit">Calcular</button>
+  </form>
 </body>
 </html>
