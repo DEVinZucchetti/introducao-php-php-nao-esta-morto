65543b68865d569fa089b7ef84513dd192dde97a
Caroline Sampaio <caroline_08022@hotmail.com>
2023-10-02 23:11:38 -0300

    refactor: atualiza titulo pagina calculadora

--- Caroline Sampaio/pages/calculadora.php
+++ Caroline Sampaio/pages/calculadora.php
@@ -2,44 +2,44 @@
 <html lang="pt-BR">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+    <style>
     .container {
-      margin: 0 auto;
-      width: 50%;
-      height: 100vh;
+        margin: 0 auto;
+        width: 50%;
+        height: 100vh;
 
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      gap: 10px;
-      padding: 10px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        gap: 10px;
+        padding: 10px;
     }
-  </style>
+    </style>
 </head>
 
 <body>
-  <form class="container" method="post" action="resultado.php">
-    <h1>Calculadora</h1>
-    <label>Valor 1:</label>
-    <input type="number" step="any" name="valor1" placeholder="Digite o primeiro valor">
-    <label>Valor 2:</label>
-    <input type="number" step="any" name="valor2" placeholder="Digite o segundo valor">
-    <label>Operação:</label>
-    <select name="operacao">
-      <option value="soma">Soma</option>
-      <option value="subtracao">Subtração</option>
-      <option value="multiplicacao">Multiplicação</option>
-      <option value="divisao">Divisão</option>
-    </select>
-    <button type="submit">Calcular preço</button>
-  </form>
+    <form class="container" method="post" action="resultado.php">
+        <h1>Calculadora</h1>
+        <label>Valor 1:</label>
+        <input type="number" step="any" name="valor1" placeholder="Digite o primeiro valor">
+        <label>Valor 2:</label>
+        <input type="number" step="any" name="valor2" placeholder="Digite o segundo valor">
+        <label>Operação:</label>
+        <select name="operacao">
+            <option value="soma">Soma</option>
+            <option value="subtracao">Subtração</option>
+            <option value="multiplicacao">Multiplicação</option>
+            <option value="divisao">Divisão</option>
+        </select>
+        <button type="submit">Calcular preço</button>
+    </form>
 
 </body>
 <footer>
-  <p>Desenvolvido por Caroline Sampaio</p>
+    <p>Desenvolvido por Caroline Sampaio</p>
 </footer>
 
 </html>
