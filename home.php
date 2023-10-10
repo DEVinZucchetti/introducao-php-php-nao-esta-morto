25986cc63eb195e40cae889ab2ee5a5c9820b51b
Jusouza182 <juliana_s_souza@estudante.sesisenai.org.br>
2023-10-09 21:15:30 -0300

    update: home

--- home.php
+++ home.php
@@ -54,7 +54,7 @@
     <div class="chat">
 
         <div class="message sent">
-            <p><span class="author">Nome do aluno</span>: Olá,  professor!  <a href="./modelo/"> Clique aqui a minha solução.</a>.</p>
+            <p><span class="author">Juliana</span>: Olá,  professor!  <a href="./juliana_ss/"> Clique aqui a minha solução.</a>.</p>
            
         </div>
         <!-- Adicione mais mensagens aqui -->
---
+++ juliana_ss/pages/imc.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Calcular IMC</title>
+</head>
+<body>
+<p>Para calcular o IMC informe: </p>
+    <form method="post" action="resultado.php">
+    <label>Peso::</label>
+        <input type="number" placeholder="Digite o peso" name="peso" />
+        <br />
+        <br />
+        <label>Altura:</label>
+        <input type="number" placeholder="Digite a altura" name="altura" />
+        <br />
+        <br />
+        <button type="submit">Calcular</button>
+    </form>
+  </body>
+</html>
